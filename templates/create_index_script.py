b24571e5bb0712e07482928d9ae5ba5a92be34f3
Andrew Lem <Andrew.Lem@ga.gov.au>
2018-12-06 14:39:36 +1100

    connection to database through python working!

--- templates/create_index_script.py
+++ templates/create_index_script.py
@@ -27,11 +27,12 @@ def create_sql_statement(templates_dict, metadata_type_ref, index_types,
 
 templates_dict = {}
 
+print('Connecting to database...')
 with psycopg2.connect(host="localhost",
                       database="datacube",
                       port=9999,
-                      username='al6701',
-                      password='') as conn:
+                      user='al6701') as conn:
+    print('Connected!')
     #     fsdetails = cur.fetchall()
     #     print(cur.fetchall())
     #
@@ -40,27 +41,28 @@ with psycopg2.connect(host="localhost",
     # total_used = 0
     # for device, blocks, used, available, percent_use, mount in fsdetails:
 
+    print('Running query...')
     with conn.cursor() as cur:
         cur.execute("""
-        SELECT * FROM agdc.dataset;
+        SELECT * FROM pg_indexes;
         """)
-
+        print('Query complete!')
         print(cur.fetchall())
 
-
-        current_metadata_type_ref = -1
-        current_index_count_max = -1
-        current_index_types = []
-        for dataset_type_ref, product_name, metadata_type_ref, index_type_count, index_types in cur:
-            if current_metadata_type_ref != metadata_type_ref:
-                current_metadata_type_ref = metadata_type_ref
-                current_index_count_max = index_type_count
-                current_index_types = str(index_types)[1:-1].split(',')
-            elif index_type_count < current_index_count_max:
-                index_types = str(index_types)[1:-1].split(',')
-                missing_indexes = [index for index in current_index_types if index not in index_types]
-
-
+        #
+        # current_metadata_type_ref = -1
+        # current_index_count_max = -1
+        # current_index_types = []
+        # for dataset_type_ref, product_name, metadata_type_ref, index_type_count, index_types in cur:
+        #     if current_metadata_type_ref != metadata_type_ref:
+        #         current_metadata_type_ref = metadata_type_ref
+        #         current_index_count_max = index_type_count
+        #         current_index_types = str(index_types)[1:-1].split(',')
+        #     elif index_type_count < current_index_count_max:
+        #         index_types = str(index_types)[1:-1].split(',')
+        #         missing_indexes = [index for index in current_index_types if index not in index_types]
+        #
+        #
 
 # metadata_type_ref = 1
 # index_attributes = 'time_lat_lon'
