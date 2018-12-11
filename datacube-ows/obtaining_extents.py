3191c53711e058170b0fd353902e270e06a25e77
Andrew Lem <Andrew.Lem@ga.gov.au>
2018-12-11 16:36:16 +1100

    outputs and records time for extents to output_filename

--- datacube-ows/obtaining_extents.py
+++ datacube-ows/obtaining_extents.py
@@ -1,7 +1,10 @@
 import psycopg2
+import time
 
 
 def main():
+    output_filename = "extent_output2.txt"
+
     with psycopg2.connect(host="localhost",
                           database="datacube",
                           port=9999,
@@ -15,24 +18,73 @@ def main():
             id_result = cur.fetchall()
 
             for id in id_result:
+                with open(output_filename, "a") as f:
+                    f.write(f"dataset_type: {id[0]}\n")
+
+                start = time.time()
 
                 cur.execute(f"""
-                select count(id) as count,
-                       dataset_type_ref,
-                       min((metadata #>> '{{extent,coord,ul,lat}}'::text[])::double precision) as min_ul_lat,
-                       max((metadata #>> '{{extent,coord,ul,lat}}'::text[])::double precision) as max_ul_lat,
-                       min((metadata #>> '{{extent,coord,ur,lat}}'::text[])::double precision) as min_ur_lat,
-                       max((metadata #>> '{{extent,coord,ur,lat}}'::text[])::double precision) as max_ur_lat,
-                       min((metadata #>> '{{extent,coord,ll,lat}}'::text[])::double precision) as min_ll_lat,
-                       max((metadata #>> '{{extent,coord,ll,lat}}'::text[])::double precision) as max_ll_lat,
-                       min((metadata #>> '{{extent,coord,lr,lat}}'::text[])::double precision) as min_lr_lat,
-                       max((metadata #>> '{{extent,coord,lr,lat}}'::text[])::double precision) as max_lr_lat
+                select (metadata #>> '{{extent,coord,ul,lat}}'::text[])::double precision as ul_lats,
+                       (metadata #>> '{{extent,coord,ur,lat}}'::text[])::double precision as ur_lats,
+                       (metadata #>> '{{extent,coord,ll,lat}}'::text[])::double precision as ll_lats,
+                       (metadata #>> '{{extent,coord,lr,lat}}'::text[])::double precision as lr_lats
                 from agdc.dataset
-                where dataset_type_ref = {id[0]}
-                group by dataset_type_ref;
+                where archived IS NULL and dataset_type_ref = {id[0]};
                 """)
+                query_done = time.time()
 
-                print(cur.fetchone())
+                product_values = cur.fetchall()
+                ul_lats = []
+                ur_lats = []
+                ll_lats = []
+                lr_lats = []
+                for product_value in product_values:
+                    ul_lats.append(product_value[0])
+                    ur_lats.append(product_value[1])
+                    ll_lats.append(product_value[2])
+                    lr_lats.append(product_value[3])
+
+                ul_lat_min = 0
+                ul_lat_max = 0
+                ur_lat_min = 0
+                ur_lat_max = 0
+                ll_lat_min = 0
+                ll_lat_max = 0
+                lr_lat_min = 0
+                lr_lat_max = 0
+                if ul_lats:
+                    ul_lat_min = min(ul_lats)
+                    ul_lat_max = max(ul_lats)
+                if ur_lats:
+                    ur_lat_min = min(ur_lats)
+                    ur_lat_max = max(ur_lats)
+                if ll_lats:
+                    ll_lat_min = min(ll_lats)
+                    ll_lat_max = max(ll_lats)
+                if lr_lats:
+                    lr_lat_min = min(lr_lats)
+                    lr_lat_max = max(lr_lats)
+
+                end = time.time()
+
+                with open(output_filename, "a") as f:
+                    if ul_lats:
+                        size = len(ul_lats)
+                    else:
+                        size = 0
+                    f.write(f"size: {size}\n")
+                    f.write(f"total_time: {end - start}\n")
+                    f.write(f"query_time: {query_done - start}\n")
+                    f.write(f"sort_time: {end - query_done}\n")
+
+                    f.write(f"ul_lat_min:{ul_lat_min}\n")
+                    f.write(f"ul_lat_max:{ul_lat_max}\n")
+                    f.write(f"ur_lat_min:{ur_lat_min}\n")
+                    f.write(f"ur_lat_max:{ur_lat_max}\n")
+                    f.write(f"ll_lat_min:{ll_lat_min}\n")
+                    f.write(f"ll_lat_max:{ll_lat_max}\n")
+                    f.write(f"lr_lat_min:{lr_lat_min}\n")
+                    f.write(f"lr_lat_max:{lr_lat_max}\n\n\n")
 
 
 if __name__ == '__main__':
