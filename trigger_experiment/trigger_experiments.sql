d9a330626ddd2d1df05e86add807bc34e7bdb6fd
Andrew Lem <Andrew.Lem@ga.gov.au>
2019-01-16 16:03:50 +1100

    trigger experiment indicates minimal impact on insert statements

--- trigger_experiment/trigger_experiments.sql
+++ trigger_experiment/trigger_experiments.sql
@@ -51,7 +51,8 @@ CREATE TRIGGER product_count
   FOR EACH ROW
 EXECUTE PROCEDURE adjust_count();
 
-CREATE TABLE agdc.extra_dataset_info as
+CREATE TABLE agdc.extra_dataset_info
+as
 select id,
        dataset_type_ref,
        LEAST((metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
@@ -69,18 +70,66 @@ select id,
        GREATEST((metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
                 (metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
                 (metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
-                (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision) as lon_greatest
+                (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision) as lon_greatest,
+       agdc.common_timestamp(metadata #>> '{extent,from_dt}'::text[])             as from_dt,
+       agdc.common_timestamp(metadata #>> '{extent,to_dt}'::text[])               as to_dt
 from agdc.dataset;
 
 CREATE TABLE agdc.extra_dataset_info
 (
-  base_id uuid PRIMARY KEY,
+  base_id          uuid PRIMARY KEY,
   dataset_type_ref smallint,
-  lat_least double precision,
-  lat_greatest double precision,
-  lon_least double precision,
-  lon_most double precision
+  lat_least        double precision,
+  lat_greatest     double precision,
+  lon_least        double precision,
+  lon_most         double precision
 );
 
 
-select * from agdc.extra_dataset_info;
+select *
+from agdc.extra_dataset_info;
+
+
+
+CREATE OR REPLACE FUNCTION extra_dataset_info_update()
+  RETURNS TRIGGER AS
+$$
+DECLARE
+BEGIN
+  IF TG_OP = 'INSERT' THEN
+    EXECUTE 'INSERT INTO agdc.extra_dataset_info(id, dataset_type_ref, ' ||
+            'lat_least, lat_greatest, lon_least, lon_greatest)' ||
+            'VALUES (''' || NEW.id || ''',' || NEW.dataset_type_ref || ',' ||
+            LEAST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision) || ',' ||
+            GREATEST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision) || ',' ||
+            LEAST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+                  (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision) || ',' ||
+            GREATEST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+                     (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision) || ',' ||
+            '''' || metadata #>> '{extent,from_dt}'::text[] || ''',' ||
+            '''' || metadata #>> '{extent,to_dt}'::text[] || ''')';
+    RETURN NEW;
+  ELSIF TG_OP = 'DELETE' THEN
+    EXECUTE 'DELETE FROM agdc.extra_dataset_info WHERE id = ''' || OLD.id || '''';
+    RETURN OLD;
+  END IF;
+END;
+$$
+  LANGUAGE 'plpgsql';
+
+CREATE TRIGGER product_extra_info
+  BEFORE INSERT OR DELETE
+  ON agdc.dataset
+  FOR EACH ROW
+EXECUTE PROCEDURE extra_dataset_info_update();
+
