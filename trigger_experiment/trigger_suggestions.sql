fb3aecce0f2436ccaa1f8d7371a78756e5f4f9a8
Andrew Lem <Andrew.Lem@ga.gov.au>
2019-01-21 11:56:46 +1100

    Modified updates statements into a single update statements Created trigger but haven't run it yet

--- trigger_experiment/trigger_suggestions.sql
+++ trigger_experiment/trigger_suggestions.sql
@@ -14,61 +14,66 @@ ALTER TABLE agdc.dataset
   ADD COLUMN to_dt TIMESTAMP WITH TIME ZONE;
 
 UPDATE agdc.dataset
-SET lat_least = LEAST((metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
-UPDATE agdc.dataset
-SET lat_greatest = GREATEST((metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
-UPDATE agdc.dataset
-SET lon_least = LEAST((metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
-                      (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
-UPDATE agdc.dataset
-SET lon_greatest = GREATEST((metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
-                            (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
-UPDATE agdc.dataset
-SET from_dt = agdc.common_timestamp(metadata #>> '{extent,from_dt}'::text[]);
-UPDATE agdc.dataset
-SET center_dt = agdc.common_timestamp(metadata #>> '{extent,center_dt}'::text[]);
-UPDATE agdc.dataset
-SET to_dt = agdc.common_timestamp(metadata #>> '{extent,to_dt}'::text[]);
+SET (lat_least,
+     lat_greatest,
+     lon_least,
+     lon_greatest,
+     from_dt,
+     center_dt,
+     to_dt)
+      = (LEAST((metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+               (metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+               (metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+               (metadata #>> '{extent,coord,ll,lat}'::text[])::double precision),
+         GREATEST((metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,ll,lat}'::text[])::double precision),
+         LEAST((metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+               (metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+               (metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+               (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision),
+         GREATEST((metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+                  (metadata #>> '{extent,coord,lr,lon}'::text[])::double precision),
+         agdc.common_timestamp(metadata #>> '{extent,from_dt}'::text[]),
+         agdc.common_timestamp(metadata #>> '{extent,center_dt}'::text[]),
+         agdc.common_timestamp(metadata #>> '{extent,to_dt}'::text[]));
 
 CREATE OR REPLACE FUNCTION dataset_info_update()
   RETURNS TRIGGER AS
 $$
 DECLARE
 BEGIN
-  IF TG_OP = 'INSERT' THEN
-    NEW.lat_least = LEAST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
-    NEW.lat_greatest = GREATEST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
-    NEW.lon_least = LEAST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
-                          (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
-    NEW.lon_greatest = GREATEST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
-                                (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
-    NEW.from_dt = agdc.common_timestamp(NEW.metadata #>> '{extent,from_dt}'::text[]);
-    NEW.center_dt = agdc.common_timestamp(metadata #>> '{extent,center_dt}'::text[]);
-    NEW.to_dt = agdc.common_timestamp(NEW.metadata #>> '{extent,to_dt}'::text[]);
-    RETURN NEW;
-  END IF;
+  NEW.lat_least = LEAST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
+  NEW.lat_greatest = GREATEST((NEW.metadata #>> '{extent,coord,ur,lat}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,lr,lat}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,ul,lat}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,ll,lat}'::text[])::double precision);
+  NEW.lon_least = LEAST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+                        (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
+  NEW.lon_greatest = GREATEST((NEW.metadata #>> '{extent,coord,ul,lon}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,ur,lon}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,ll,lon}'::text[])::double precision,
+                              (NEW.metadata #>> '{extent,coord,lr,lon}'::text[])::double precision);
+  NEW.from_dt = agdc.common_timestamp(NEW.metadata #>> '{extent,from_dt}'::text[]);
+  NEW.center_dt = agdc.common_timestamp(metadata #>> '{extent,center_dt}'::text[]);
+  NEW.to_dt = agdc.common_timestamp(NEW.metadata #>> '{extent,to_dt}'::text[]);
+  RETURN NEW;
 END;
 $$
   LANGUAGE 'plpgsql';
 
 
+-- todo: run this trigger creation
+CREATE TRIGGER dataset_info_trigger
+  BEFORE INSERT
+  ON agdc.dataset
+  FOR EACH ROW
+EXECUTE PROCEDURE dataset_info_update();
